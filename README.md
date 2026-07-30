ff444cb572266084cc71e0d761fd093b57d6a8e2
Rachit Kumar Verma <155641117+rachitvrma@users.noreply.github.com>
2026-07-30 14:28:51 +0530

    migrate from direnv+flake to devenv

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Solutions for the questions in Reema Thareja's DSA in C
->[!NOTE]
->This repository is solely meant for my own pracitce,
->This repo does not accept any issues. It may be used as 
->a help
+
+> [!NOTE]
+> This repository is solely meant for my own practice,
+> This repo does not accept any issues. It may be used as
+> a help
---
+++ devenv.nix
@@ -0,0 +1,35 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/languages/
+  languages = {
+    c = {
+      enable = true;
+      lsp.package = pkgs.clang-tools;
+    };
+    cplusplus = {
+      enable = true;
+      lsp.package = pkgs.clang-tools;
+    };
+  };
+
+  # https://devenv.sh/basics/
+  enterShell = ''
+    echo "c/c++ devenv ready: $(gcc --dumpversion), $(cmake --version | head -1)"
+  '';
+
+  packages = with pkgs; [
+    gf
+    cppcheck
+  ];
+
+}
